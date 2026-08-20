e121649d7e894d5e415dc34bd540d482516a379a
loso0017 <bloso1244@gmail.com>
2026-08-20 16:41:44 -0500

    Add error checking to ApiClient

--- lib/data/services/api/api_client.dart
+++ lib/data/services/api/api_client.dart
@@ -13,28 +13,33 @@ class ApiClient {
     double long,
     int resultsCount,
   ) async {
-    final response = await _client.postUri(
-      Uri.parse('$Env.baseUrl/data'),
-      options: Options(
-        headers: {
-          'Content-Type': 'application/json',
-          'X-Goog-FieldMask':
-              'places.displayName,places.types,places.formattedAddress',
-          'X-Goog-Api-Key': Env.apiKey,
-        },
-      ),
-      data: {
-        'includedTypes': ['restaurant'],
-        'maxResultCount': resultsCount,
-        'locationRestriction': {
-          'circle': {
-            'center': {'latitude': lat, 'longitude': long},
+    try {
+      final response = await _client.postUri(
+        Uri.parse('$Env.baseUrl/data'),
+        options: Options(
+          headers: {
+            'Content-Type': 'application/json',
+            'X-Goog-FieldMask':
+                'places.displayName,places.types,places.formattedAddress',
+            'X-Goog-Api-Key': Env.apiKey,
           },
-          'radius': radius,
+        ),
+        data: {
+          'includedTypes': ['restaurant'],
+          'maxResultCount': resultsCount,
+          'locationRestriction': {
+            'circle': {
+              'center': {'latitude': lat, 'longitude': long},
+            },
+            'radius': radius,
+          },
         },
-      },
-    );
-    final result = response.data['places'] as List;
-    return result.map((json) => Restaurant.fromPlacesApiJson(json)).toList();
+      );
+      final result = response.data['places'] as List;
+      return result.map((json) => Restaurant.fromPlacesApiJson(json)).toList();
+    } catch (err) {
+      print('API error: $err');
+      return [];
+    }
   }
 }
