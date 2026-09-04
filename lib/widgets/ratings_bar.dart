98f5e5a395657508e45af073fbdbf9e121ba02cf
loso0017 <bloso1244@gmail.com>
2026-09-04 15:51:23 -0500

    Feat: Add RatingsBar widget

---
+++ lib/widgets/ratings_bar.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:randomized_restaurant/ui/core/theme/theme.dart';
+
+class RatingsBar extends StatelessWidget {
+  new({
+    super.key,
+    required this.rating,
+    required this.reviewCount,
+    this.fontSize,
+    this.style,
+    this.starColor,
+  }) : assert(rating >= 0 && rating <= 5),
+       assert(reviewCount >= 0);
+
+  double rating;
+  int reviewCount;
+  double? fontSize;
+  TextStyle? style;
+  Color? starColor;
+
+  @override
+  Widget build(BuildContext context) {
+    final ColorScheme theme = Theme.of(context).colorScheme;
+    final TextTheme textTheme = Theme.of(context).textTheme;
+
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        RichText(
+          text: TextSpan(
+            text: rating.toString(),
+            style: textTheme.titleMedium?.copyWith(
+              color: theme.onSurfaceVariant,
+            ),
+
+            children: [
+              WidgetSpan(
+                child: Icon(
+                  Icons.star,
+                  color: MaterialTheme.ratingStar.value,
+                  size: (textTheme.titleLarge?.fontSize)! - 3.0,
+                ),
+              ),
+              TextSpan(text: ' (${reviewCount.toString()})'),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
