a16e991c6ea49f9e68d71708b57b3b1305ebfbda
loso0017 <bloso1244@gmail.com>
2026-09-04 16:13:23 -0500

    feat(result screen): update appbar to have title and second action

--- lib/ui/screens/result_screen.dart
+++ lib/ui/screens/result_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:randomized_restaurant/widgets/restaurant_card_view.dart';
 
@@ -6,9 +8,45 @@ class ResultScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final ColorScheme theme = Theme.of(context).colorScheme;
+    final TextTheme textTheme = Theme.of(context).textTheme;
+
+    // TODO fix scaling of toolbar icons
+    // TODO add re-roll button
     return Scaffold(
-      appBar: AppBar(title: const Text('Result')),
-      body: Align(alignment: Alignment.topCenter, child: RestaurantCardView()),
+      appBar: AppBar(
+        titleSpacing: 10,
+        centerTitle: true,
+        toolbarHeight: min(MediaQuery.heightOf(context) * 0.1, 52),
+        iconTheme: IconThemeData(applyTextScaling: true),
+
+        actions: [
+          Center(
+            widthFactor: 1.38,
+            child: FittedBox(
+              fit: BoxFit.scaleDown,
+              child: IconButton(
+                onPressed: () => print("Not ready yet"),
+                icon: Icon(Icons.person_off_outlined),
+              ),
+            ),
+          ),
+        ],
+        title: FittedBox(
+          fit: BoxFit.scaleDown,
+          child: Text(
+            'How\'s this?',
+            style: textTheme.headlineLarge,
+            maxLines: 1,
+          ),
+        ),
+      ),
+
+      body: ListView(
+        padding: EdgeInsets.only(top: 8),
+
+        children: [RestaurantCardView()],
+      ),
     );
   }
 }
