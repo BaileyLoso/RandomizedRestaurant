6f7550901fab7514fe08ce6b7df561d4a3eb1587
loso0017 <bloso1244@gmail.com>
2026-09-04 16:03:28 -0500

    Feat: Add ResultPhoto widget

---
+++ lib/widgets/result_photo.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:randomized_restaurant/data/repositories/photo_repository.dart';
+
+class ResultPhoto extends ConsumerStatefulWidget {
+  final String name;
+  const new({super.key, required this.name});
+
+  @override
+  ConsumerState<ResultPhoto> createState() => _ResultPhotoState();
+}
+
+class _ResultPhotoState extends ConsumerState<ResultPhoto> {
+  @override
+  Widget build(BuildContext context) {
+    final ColorScheme theme = Theme.of(context).colorScheme;
+
+    var photoMap = ref.watch(photoRepositoryProvider);
+    var entry = photoMap[widget.name];
+    return switch (entry) {
+      PhotoCacheEntry(photo: _, accessTime: _) => Container(
+        decoration: BoxDecoration(
+          // gradient: LinearGradient(
+          //   colors: [Colors.transparent, Colors.black],
+          //   begin: AlignmentGeometry.center,
+          //   end: AlignmentGeometry.bottomCenter,
+          // ),
+          image: DecorationImage(
+            image: NetworkImage(entry.photo.uri),
+            fit: BoxFit.cover,
+          ),
+        ),
+        foregroundDecoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.transparent, theme.onSurface],
+            begin: AlignmentGeometry.center,
+            end: AlignmentGeometry.bottomCenter,
+          ),
+          backgroundBlendMode: BlendMode.xor,
+        ),
+      ),
+      null => SizedBox.expand(child: Container()),
+    };
+  }
+}
