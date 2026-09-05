aadf3fba9aa3773ec8ccaef0a9cbadb5b226e824
loso0017 <bloso1244@gmail.com>
2026-09-04 21:28:22 -0500

    feat: move next and back buttons on results page to bottom of screen

--- lib/ui/screens/result_screen.dart
+++ lib/ui/screens/result_screen.dart
@@ -1,7 +1,8 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:randomized_restaurant/widgets/next_button.dart';
+import 'package:randomized_restaurant/widgets/next_result_button.dart';
+import 'package:randomized_restaurant/widgets/previous_result_button.dart';
 import 'package:randomized_restaurant/widgets/restaurant_card_view.dart';
 
 class ResultScreen extends StatelessWidget {
@@ -9,7 +10,6 @@ class ResultScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final ColorScheme theme = Theme.of(context).colorScheme;
     final TextTheme textTheme = Theme.of(context).textTheme;
 
     // TODO fix scaling of toolbar icons
@@ -24,7 +24,7 @@ class ResultScreen extends StatelessWidget {
           Center(
             widthFactor: 1.38,
             child: FittedBox(
-              fit: BoxFit.scaleDown,
+              fit: .scaleDown,
               child: IconButton(
                 onPressed: () => print("Not ready yet"),
                 icon: Icon(Icons.person_off_outlined),
@@ -33,7 +33,7 @@ class ResultScreen extends StatelessWidget {
           ),
         ],
         title: FittedBox(
-          fit: BoxFit.scaleDown,
+          fit: .scaleDown,
           child: Text(
             'How\'s this?',
             style: textTheme.headlineLarge,
@@ -42,10 +42,30 @@ class ResultScreen extends StatelessWidget {
         ),
       ),
 
-      body: ListView(
-        padding: EdgeInsets.only(top: 8),
+      body: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              padding: const .only(top: 8),
+              child: RestaurantCardView(),
+            ),
+          ),
+          SafeArea(
+            child: FittedBox(
+              fit: .scaleDown,
 
-        children: [RestaurantCardView(), NextResultButton()],
+              child: Padding(
+                padding: const .only(bottom: 12.0),
+                child: Row(
+                  spacing: 16,
+                  mainAxisAlignment: .center,
+
+                  children: [PreviousResultButton(), NextResultButton()],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
