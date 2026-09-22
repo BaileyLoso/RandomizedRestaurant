9d123db07c0c037231e84be2690b3b7271191695
Bailey <bloso1244@gmail.com>
2026-09-22 12:39:30 -0500

    feat(randomize button): change behavior so button changes loading state before switching page

--- lib/widgets/randomize_button.dart
+++ lib/widgets/randomize_button.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:go_router/go_router.dart';
 import 'package:randomized_restaurant/ui/view_models/result_view_model.dart';
 
@@ -11,6 +10,13 @@ class RandomizeButton extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final theme = Theme.of(context).colorScheme;
     final textTheme = Theme.of(context).textTheme;
+
+    // Listen for when results start loading to push
+    ref.listen(resultViewModelProvider, (previous, next) {
+      if (previous?.isLoading != true && next.isLoading) {
+        context.pushNamed('result');
+      }
+    });
 
     final ButtonStyle style = ButtonStyle(
       backgroundColor: WidgetStateProperty.fromMap(
@@ -35,28 +41,8 @@ class RandomizeButton extends ConsumerWidget {
           width: 250,
           child: ElevatedButton(
             style: style,
-            onPressed: () async {
-              var serviceEnabled = await Geolocator.isLocationServiceEnabled();
-              if (!serviceEnabled) {
-                return Future.error('Location services disabled');
-              }
-
-              var permission = await Geolocator.checkPermission();
-              if (permission == LocationPermission.denied) {
-                permission = await Geolocator.requestPermission();
-                if (permission == LocationPermission.denied ||
-                    permission == LocationPermission.deniedForever) {
-                  return Future.error('Location permissions are denied');
-                }
-              }
-
-              Position coords = await Geolocator.getCurrentPosition();
-              await ref
-                  .read(resultViewModelProvider.notifier)
-                  .load(latitude: coords.latitude, longitude: coords.longitude);
-              if (context.mounted) {
-                context.pushNamed('result');
-              }
+            onPressed: () {
+              ref.read(resultViewModelProvider.notifier).load();
             },
             child: Center(
               child: Text(
